773823c0df9d45b12e135a01825299447c00e8e4
Nick Collins <nick@vividx.co.uk>
2013-12-30 21:23:46 +0000

    add `repos` table to database

--- setup.py
+++ setup.py
@@ -5,9 +5,19 @@ conn = sqlite3.connect('database.db')
 c = conn.cursor()
 
 c.execute("""
-          CREATE TABLE users (
+          CREATE TABLE IF NOT EXISTS users (
           name TEXT PRIMARY KEY,
           repos_url TEXT
+          );
+          """)
+
+c.execute("""
+          CREATE TABLE IF NOT EXISTS repos (
+          name TEXT, 
+          owner TEXT,
+          url TEXT,
+          language TEXT,
+          PRIMARY KEY (name, owner)
           );
           """)
 
