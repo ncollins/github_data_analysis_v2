ca1967ffd7b762b57109da183fd2fb32d2df5774
Nick Collins <nick@vividx.co.uk>
2014-01-03 12:46:42 -0500

    refactors `FetchUrlWorker`

--- scraper/src/main.py
+++ scraper/src/main.py
@@ -28,12 +28,10 @@ def get_pages(url, max_pages=100):
     data = []
     for i in range(max_pages):
         print('GET: {}'.format(url.format(i)))
-        new_data = get_page(url.format(i))
-        data.extend(new_data)
-        if len(new_data) < 30:
+        ds = get_page(url.format(i))
+        data.extend(ds)
+        if len(ds) < 30:
             break
-    # remove cases where we didn't get an object
-    #return [d for d in data if not isinstance(d, str)]
     return data
 
 
@@ -51,47 +49,55 @@ class FetchUrlWorker(threading.Thread):
             page_type = job[0]
             if page_type == 'user':
                 url = job[1]
-                data = get_page(url)
-                try:
-                    login = data.get('login', None)
-                    repos_url = data.get('repos_url', None)
-                    if login:
-                        self.db_queue.put(('users', (login, repos_url)))
-                    else:
-                        print('No "login" for {0}'.format(url))
-                    if repos_url:
-                        self.download_queue.put(('repos', repos_url))
-                except:
-                    print('problem with {0}'.format(login))
+                self._fetch_user(url)
             elif page_type == 'repos':
                 url = job[1]
-                raw_data = get_pages(url)
-                data = []
-                for r in raw_data:
-                    data.append((r['name'], # name
-                                 r['owner']['login'], # owner
-                                 r['url'],
-                                 r['language'],
-                                 1 if r['fork'] == True else 0,
-                                 r['contributors_url']))
-                    if r['fork'] == False:
-                        self.download_queue.put(('contributors', r['contributors_url'], r['name']))
-                if data:
-                    self.db_queue.put(('repos', data))
+                self._fetch_repos(url)
             elif page_type == 'contributors':
-                url = job[1]
-                repo = job[2]
-                raw_data = get_pages(url)
-                data = []
-                for r in raw_data:
-                    data.append((repo,
-                                 r['login'],
-                                 int(r['contributions'])))
-                if data:
-                    self.db_queue.put(('contributors', data))
-                
+                url, repo_name = job[1], job[2]
+                self._fetch_contributors(url, repo_name)
+            else:
+                print('Unrecognized page type "{0}".'.format(page_type))
             self.download_queue.task_done() #signals to download_queue job is done
 
+    def _fetch_user(self, url):
+        data = get_page(url)
+        try:
+            login = data.get('login', None)
+            repos_url = data.get('repos_url', None)
+            if login:
+                self.db_queue.put(('users', (login, repos_url)))
+                if repos_url:
+                    self.download_queue.put(('repos', repos_url))
+            else:
+                print('No "login" for {0}'.format(url))
+        except:
+            print('problem with {0}'.format(login))
+
+    def _fetch_repos(self, url):
+        repos = get_pages(url)
+        data = []
+        for r in repos:
+            data.append((r['name'],
+                         r['owner']['login'],
+                         r['url'],
+                         r['language'],
+                         1 if r['fork'] == True else 0,
+                         r['contributors_url']))
+            if r['fork'] == False:
+                self.download_queue.put(('contributors', r['contributors_url'], r['name']))
+        if data:
+            self.db_queue.put(('repos', data)) 
+
+    def _fetch_contributors(self, url, repo_name):
+        contributors = get_pages(url)
+        data = []
+        for c in contributors:
+            data.append((repo_name,
+                         c['login'],
+                         int(c['contributions'])))
+        if data:
+            self.db_queue.put(('contributors', data))
 
 class DbWorker(threading.Thread):
     def __init__(self, queue):
@@ -100,7 +106,6 @@ class DbWorker(threading.Thread):
 
     def run(self):
         conn = sqlite3.connect('../../database.db')
-        #c = conn.cursor()
         while True:
             table, data = self.queue.get()
             try:
