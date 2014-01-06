47c998e163a9e90afa3e2c37a41d281dba9ef99e
Nick Collins <nick@vividx.co.uk>
2014-01-05 19:31:55 -0500

    changes table keys to integer ids
    adds `contributors` table and `repos_with_owner` view

--- setup.py
+++ setup.py
@@ -6,19 +6,40 @@ c = conn.cursor()
 
 c.execute("""
           CREATE TABLE IF NOT EXISTS users (
-          name TEXT PRIMARY KEY,
+          id INTEGER PRIMARY KEY,
+          login TEXT KEY,
           repos_url TEXT
           );
           """)
 
 c.execute("""
           CREATE TABLE IF NOT EXISTS repos (
-          name TEXT, 
-          owner TEXT,
+          id INTEGER PRIMARY KEY,
+          user_id INTEGER KEY,
+          name TEXT KEY,
           url TEXT,
           language TEXT,
-          PRIMARY KEY (name, owner)
+          fork INTEGER,
+          contributors_url TEXT
           );
+          """)
+
+c.execute("""
+          CREATE TABLE IF NOT EXISTS contributors (
+          repo_id INTEGER KEY,
+          user_id INTEGER KEY,
+          contributions INTEGER,
+          PRIMARY KEY (repo_id, user_id)
+          );
+          """)
+
+c.execute("""
+          CREATE VIEW IF NOT EXISTS repos_with_owner
+          AS
+          SELECT
+          repos.id AS repo_id, repos.name AS repo_name,
+          users.id as repo_owner_id, users.login as repo_owner_login
+          FROM repos LEFT JOIN users ON repos.user_id = users.id;
           """)
 
 conn.commit()
